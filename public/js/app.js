e705a4da74979388d2c2ffad44fe7c86f20475b1
Tobin Skinner <tobin.skinner@gmail.com>
2013-10-17 18:43:49 -0400

    Committing most recent version.

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,37 @@ config(['$routeProvider',
       .when('/people', {
         templateUrl: 'partials/people.html'
       })
+      .when('/contact', {
+        templateUrl: 'partials/contact.html'
+      })
       .otherwise({
         redirectTo: '/home'
       });
   }
 ])
-  .controller('LingSyncWebSiteController', function($scope) {
+  .controller('LingSyncWebSiteController', function($scope, $timeout) {
     $scope.navigateToRoute = function(route) {
       window.location.assign("#/" + route);
-    }
+    };
 
     $scope.divToShow = function(div) {
       $scope.contentToShow = div;
-    }
+    };
+
+    $scope.navigateToHome = function() {
+      $scope.pageLoaded = false;
+      $scope.divToShow('welcome');
+      window.location.assign("#/home");
+      var hideInitialDivTransition = $timeout(function() {
+        $scope.pageLoaded = true;
+      }, 1000);
+    };
 
     $scope.divToShow('welcome');
+
+    $scope.$on('$viewContentLoaded', function() {
+      var hideInitialDivTransition = $timeout(function() {
+        $scope.pageLoaded = true;
+      }, 1000);
+    });
   });
